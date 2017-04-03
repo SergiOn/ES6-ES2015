3ffb831a09b09092b65c6995d8f79b38ce1fdd33
SergiOn <onishhenko@gmail.com>
2017-04-03 19:42:40 +0300

    #10 inheritance # expand classes

--- src/inheritance.js
+++ src/inheritance.js
@@ -1,8 +1,26 @@
 class Task {
     constructor(title) {
-        this.title = title;
+        this._title = title;
         this.done = false;
+        Task.count += 2;
         console.log('Create Task');
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    static getDefaultTitle() {
+        return 'Task';
+    }
+
+    complete() {
+        this.done = true;
+        console.log(`Task "${this.title}" complete`);
     }
 }
 
@@ -12,14 +30,27 @@ class SubTask extends Task {
         this.parent = parent;
         console.log('Create SubTask');
     }
+
+    complete() {
+        super.complete();
+        // this.done = true;
+        console.log(`SubTask "${this.title}" complete`);
+    }
 }
+
+Task.count = 1;
 
 let task = new Task('Learn JavaScript');
 let subTask = new SubTask('Learn ES6', task);
 
+task.complete();
+subTask.complete();
+
 console.log(task);
 console.log(subTask);
+console.log(SubTask.count);
 
 console.log(subTask instanceof SubTask);
 console.log(subTask instanceof Task);
 
+console.log(SubTask.getDefaultTitle());
