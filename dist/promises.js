d406e9247e877990ba886b953edb36f8fac18818
SergiOn <Onishhenko@gmail.com>
2017-04-30 13:10:14 +0300

    #15 promises # callback

--- dist/promises.js
+++ dist/promises.js
@@ -1,32 +1,26 @@
+'use strict';
+
 function applyForVisa(documents, resolve, reject) {
     console.log('Processing of an application');
     setTimeout(function () {
-        let visa = {};
+        var visa = {};
         Math.random() > 0.5 ? resolve(visa) : reject('The visa is denied: lack of documents');
     }, 2000);
 }
 
-function bookHotel(documents, resolve, reject) {
-    
-}
+function bookHotel(documents, resolve, reject) {}
 
-function buyTickets(documents, resolve, reject) {
-    
-}
+function buyTickets(documents, resolve, reject) {}
 
 applyForVisa({}, function (visa) {
     console.info('Visa received');
     bookHotel(visa, function (reservation) {
-        buyTickets(reservation, function (reservation) {
-
-        }, function (reason) {
+        buyTickets(reservation, function (reservation) {}, function (reason) {
             console.error(reason);
         });
-        
     }, function (reason) {
         console.error(reason);
     });
-
 }, function (reason) {
     console.error(reason);
 });
