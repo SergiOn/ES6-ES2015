02e34af0197c4729b7f6e2fbddcdc97592ab4a87
SergiOn <Onishhenko@gmail.com>
2017-04-30 15:13:16 +0300

    #15 promises # use promise

--- dist/promises.js
+++ dist/promises.js
@@ -1,26 +1,42 @@
 'use strict';
 
-function applyForVisa(documents, resolve, reject) {
+function applyForVisa(documents) {
     console.log('Processing of an application');
-    setTimeout(function () {
-        var visa = {};
-        Math.random() > 0.5 ? resolve(visa) : reject('The visa is denied: lack of documents');
-    }, 2000);
+
+    var promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            var visa = { visa: true };
+            Math.random() > 0.4 ? resolve(visa) : reject('The visa is denied: lack of documents');
+        }, 2000);
+    });
+    return promise;
 }
 
-function bookHotel(documents, resolve, reject) {}
-
-function buyTickets(documents, resolve, reject) {}
-
-applyForVisa({}, function (visa) {
-    console.info('Visa received');
-    bookHotel(visa, function (reservation) {
-        buyTickets(reservation, function (reservation) {}, function (reason) {
-            console.error(reason);
-        });
-    }, function (reason) {
-        console.error(reason);
+function bookHotel(documents) {
+    console.log(documents);
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.info('book hotel');
+            resolve('hotel');
+        }, 2000);
     });
-}, function (reason) {
-    console.error(reason);
+}
+
+function buyTickets(documents) {
+    console.log(documents);
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.info('buy tickets');
+            resolve('tickets');
+        }, 2000);
+    });
+}
+
+applyForVisa({}).then(function (visa) {
+    console.info('Visa received');
+    return visa;
+}).then(bookHotel).then(buyTickets).then(function (res) {
+    return console.log(res);
+}).catch(function (error) {
+    return console.error(error);
 });
--- src/promises.js
+++ src/promises.js
@@ -1,32 +1,42 @@
-function applyForVisa(documents, resolve, reject) {
+function applyForVisa(documents) {
     console.log('Processing of an application');
-    setTimeout(function () {
-        let visa = {};
-        Math.random() > 0.5 ? resolve(visa) : reject('The visa is denied: lack of documents');
-    }, 2000);
-}
 
-function bookHotel(documents, resolve, reject) {
-    
-}
-
-function buyTickets(documents, resolve, reject) {
-    
-}
-
-applyForVisa({}, function (visa) {
-    console.info('Visa received');
-    bookHotel(visa, function (reservation) {
-        buyTickets(reservation, function (reservation) {
-
-        }, function (reason) {
-            console.error(reason);
-        });
-        
-    }, function (reason) {
-        console.error(reason);
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(function () {
+            let visa = {visa: true};
+            Math.random() > 0.4 ? resolve(visa) : reject('The visa is denied: lack of documents');
+        }, 2000);
     });
+    return promise;
+}
 
-}, function (reason) {
-    console.error(reason);
-});
+function bookHotel(documents) {
+    console.log(documents);
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            console.info('book hotel');
+            resolve('hotel');
+        }, 2000);
+    });
+}
+
+function buyTickets(documents) {
+    console.log(documents)
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            console.info('buy tickets');
+            resolve('tickets');
+        }, 2000);
+    });
+}
+
+applyForVisa({})
+    .then(
+        (visa) => {
+            console.info('Visa received');
+            return visa;
+        })
+    .then(bookHotel)
+    .then(buyTickets)
+    .then((res) => console.log(res))
+    .catch((error) => console.error(error));
