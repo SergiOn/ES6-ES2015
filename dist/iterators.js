eeee56093e9b374d0bb77060facf11264dc85d76
SergiOn <Onishhenko@gmail.com>
2017-05-01 16:39:55 +0300

    #17 iterators # id

--- dist/iterators.js
+++ dist/iterators.js
@@ -1,16 +1,56 @@
-'use strict';
+"use strict";
 
-var xmen = ['Cyclops', 'Wolverine', 'Rogue'];
+// let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
 
 // for (let xman of xmen) {
 //     console.log(xman);
 // }
 
 
-var iterator = xmen[Symbol.iterator]();
-var next = iterator.next();
+// let iterator = xmen[Symbol.iterator]();
+// let next = iterator.next();
+//
+// while (!next.done) {
+//     console.log(next.value);
+//     next = iterator.next();
+// }
 
-while (!next.done) {
-    console.log(next.value);
-    next = iterator.next();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var idGenerator = _defineProperty({}, Symbol.iterator, function () {
+    var id = 1;
+    return {
+        next: function next() {
+            var value = id < 100 ? id++ : undefined;
+            var done = !value;
+
+            return { value: value, done: done };
+        }
+    };
+});
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = idGenerator[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var id = _step.value;
+
+        console.log(id);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
 }
--- src/iterators.js
+++ src/iterators.js
@@ -1,14 +1,36 @@
-let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
+"use strict";
+
+// let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
 
 // for (let xman of xmen) {
 //     console.log(xman);
 // }
 
 
-let iterator = xmen[Symbol.iterator]();
-let next = iterator.next();
+// let iterator = xmen[Symbol.iterator]();
+// let next = iterator.next();
+//
+// while (!next.done) {
+//     console.log(next.value);
+//     next = iterator.next();
+// }
 
-while (!next.done) {
-    console.log(next.value);
-    next = iterator.next();
+
+let idGenerator = {
+    [Symbol.iterator]() {
+        let id = 1;
+        return {
+            next() {
+                let value = id < 100 ? id++ : undefined;
+                let done = !value;
+
+                return { value, done };
+            }
+        }
+    }
+};
+
+
+for (let id of idGenerator) {
+    console.log(id);
 }
