17232d20ba64022a35f31b8d5f159ef9ab326635
SergiOn <Onishhenko@gmail.com>
2017-05-01 21:23:14 +0300

    #18 generators-creating # yield, return, throw examples

--- dist/generators-creating.js
+++ dist/generators-creating.js
@@ -34,49 +34,83 @@ console.log(iterator.next(4));
 console.log(iterator.next(5));
 */
 
-var _marked = [generator].map(regeneratorRuntime.mark);
+var _marked = [generatorArray, generator, generatorReturn, generatorThrow, generatorTryCatch].map(regeneratorRuntime.mark);
 
-function generator() {
-    var arr;
-    return regeneratorRuntime.wrap(function generator$(_context) {
+function generatorArray() {
+    return regeneratorRuntime.wrap(function generatorArray$(_context) {
         while (1) {
             switch (_context.prev = _context.next) {
                 case 0:
                     _context.next = 2;
-                    return 110;
+                    return [510, 520, 530];
 
                 case 2:
-                    _context.t0 = _context.sent;
-                    _context.next = 5;
-                    return 120;
+                    return _context.delegateYield([510, 520, 530], 't0', 3);
 
-                case 5:
-                    _context.t1 = _context.sent;
-                    _context.next = 8;
-                    return 130;
-
-                case 8:
-                    _context.t2 = _context.sent;
-                    arr = [_context.t0, _context.t1, _context.t2];
-
-                    console.log('arr:', arr);
-                    console.log('arr:', arr[2]);
-                    _context.next = 14;
-                    return 30;
-
-                case 14:
-                    return _context.delegateYield([210, 220, 230], 't3', 15);
-
-                case 15:
-                    _context.next = 17;
-                    return 40;
-
-                case 17:
+                case 3:
                 case 'end':
                     return _context.stop();
             }
         }
     }, _marked[0], this);
+}
+
+function generator() {
+    var arr;
+    return regeneratorRuntime.wrap(function generator$(_context2) {
+        while (1) {
+            switch (_context2.prev = _context2.next) {
+                case 0:
+                    _context2.next = 2;
+                    return 110;
+
+                case 2:
+                    _context2.t0 = _context2.sent;
+                    _context2.next = 5;
+                    return 120;
+
+                case 5:
+                    _context2.t1 = _context2.sent;
+                    _context2.next = 8;
+                    return 130;
+
+                case 8:
+                    _context2.t2 = _context2.sent;
+                    arr = [_context2.t0, _context2.t1, _context2.t2];
+
+                    console.log('arr:', arr);
+                    console.log('arr:', arr[2]);
+                    _context2.next = 14;
+                    return 30;
+
+                case 14:
+                    _context2.next = 16;
+                    return [210, 220, 230];
+
+                case 16:
+                    return _context2.delegateYield([210, 220, 230], 't3', 17);
+
+                case 17:
+                    _context2.next = 19;
+                    return 40;
+
+                case 19:
+                    _context2.next = 21;
+                    return generatorArray();
+
+                case 21:
+                    return _context2.delegateYield(generatorArray(), 't4', 22);
+
+                case 22:
+                    _context2.next = 24;
+                    return 50;
+
+                case 24:
+                case 'end':
+                    return _context2.stop();
+            }
+        }
+    }, _marked[1], this);
 }
 
 var iterator = generator();
@@ -90,3 +124,119 @@ console.log(iterator.next(7));
 console.log(iterator.next(8));
 console.log(iterator.next(9));
 console.log(iterator.next(10));
+console.log(iterator.next(11));
+console.log(iterator.next(12));
+console.log(iterator.next(13));
+console.log(iterator.next(14));
+console.log(iterator.next(15));
+console.log(iterator.next(16));
+console.log(iterator.next(17));
+
+console.log('generatorReturn');
+
+function generatorReturn() {
+    return regeneratorRuntime.wrap(function generatorReturn$(_context3) {
+        while (1) {
+            switch (_context3.prev = _context3.next) {
+                case 0:
+                    _context3.next = 2;
+                    return 1010;
+
+                case 2:
+                    _context3.next = 4;
+                    return 1020;
+
+                case 4:
+                    _context3.next = 6;
+                    return 1030;
+
+                case 6:
+                case 'end':
+                    return _context3.stop();
+            }
+        }
+    }, _marked[2], this);
+}
+
+var iteratorReturn = generatorReturn();
+console.log(iteratorReturn.next(1));
+console.log(iteratorReturn.return(2));
+console.log(iteratorReturn.next(3));
+console.log(iteratorReturn.next(4));
+
+console.log('generatorThrow');
+
+function generatorThrow() {
+    return regeneratorRuntime.wrap(function generatorThrow$(_context4) {
+        while (1) {
+            switch (_context4.prev = _context4.next) {
+                case 0:
+                    _context4.next = 2;
+                    return 2010;
+
+                case 2:
+                    _context4.next = 4;
+                    return 2020;
+
+                case 4:
+                    _context4.next = 6;
+                    return 2030;
+
+                case 6:
+                case 'end':
+                    return _context4.stop();
+            }
+        }
+    }, _marked[3], this);
+}
+
+var iteratorThrow = generatorThrow();
+console.log(iteratorThrow.next(1));
+// console.log(iteratorThrow.throw('error'));
+console.log(iteratorThrow.throw(new Error('error')));
+console.log(iteratorThrow.next(3));
+console.log(iteratorThrow.next(4));
+
+console.log('generatorTryCatch');
+// need comment: console.log(iteratorThrow.throw(new Error('error'))); :103
+
+function generatorTryCatch() {
+    return regeneratorRuntime.wrap(function generatorTryCatch$(_context5) {
+        while (1) {
+            switch (_context5.prev = _context5.next) {
+                case 0:
+                    _context5.prev = 0;
+                    _context5.next = 3;
+                    return 3010;
+
+                case 3:
+                    _context5.next = 5;
+                    return 3020;
+
+                case 5:
+                    _context5.next = 7;
+                    return 3030;
+
+                case 7:
+                    _context5.next = 12;
+                    break;
+
+                case 9:
+                    _context5.prev = 9;
+                    _context5.t0 = _context5['catch'](0);
+
+                    console.error(_context5.t0);
+
+                case 12:
+                case 'end':
+                    return _context5.stop();
+            }
+        }
+    }, _marked[4], this, [[0, 9]]);
+}
+
+var iteratorTryCatch = generatorTryCatch();
+console.log(iteratorTryCatch.next(1));
+console.log(iteratorTryCatch.throw(new Error('error')));
+console.log(iteratorTryCatch.next(3));
+console.log(iteratorTryCatch.next(4));
--- src/generators-creating.js
+++ src/generators-creating.js
@@ -35,13 +35,22 @@ console.log(iterator.next(5));
 */
 
 
+function* generatorArray() {
+    yield [510, 520, 530];
+    yield* [510, 520, 530];
+}
+
 function* generator() {
     let arr = [yield 110, yield 120, yield 130];
     console.log('arr:', arr);
     console.log('arr:', arr[2]);
     yield 30;
+    yield [210, 220, 230];
     yield* [210, 220, 230];
     yield 40;
+    yield generatorArray();
+    yield* generatorArray();
+    yield 50;
 }
 
 let iterator = generator();
@@ -55,3 +64,62 @@ console.log(iterator.next(7));
 console.log(iterator.next(8));
 console.log(iterator.next(9));
 console.log(iterator.next(10));
+console.log(iterator.next(11));
+console.log(iterator.next(12));
+console.log(iterator.next(13));
+console.log(iterator.next(14));
+console.log(iterator.next(15));
+console.log(iterator.next(16));
+console.log(iterator.next(17));
+
+
+
+console.log('generatorReturn');
+
+function* generatorReturn() {
+    yield 1010;
+    yield 1020;
+    yield 1030; // skip
+}
+
+let iteratorReturn = generatorReturn();
+console.log(iteratorReturn.next(1));
+console.log(iteratorReturn.return(2));
+console.log(iteratorReturn.next(3));
+console.log(iteratorReturn.next(4));
+
+
+console.log('generatorThrow');
+
+function* generatorThrow() {
+    yield 2010;
+    yield 2020; // skip with error
+    yield 2030;
+}
+
+let iteratorThrow = generatorThrow();
+console.log(iteratorThrow.next(1));
+// console.log(iteratorThrow.throw('error'));
+console.log(iteratorThrow.throw(new Error('error')));
+console.log(iteratorThrow.next(3));
+console.log(iteratorThrow.next(4));
+
+
+console.log('generatorTryCatch');
+// need comment: console.log(iteratorThrow.throw(new Error('error'))); :103
+
+function* generatorTryCatch() {
+    try {
+        yield 3010;
+        yield 3020; // doesn't skip with error, but return undefined
+        yield 3030;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+let iteratorTryCatch = generatorTryCatch();
+console.log(iteratorTryCatch.next(1));
+console.log(iteratorTryCatch.throw(new Error('error')));
+console.log(iteratorTryCatch.next(3));
+console.log(iteratorTryCatch.next(4));
