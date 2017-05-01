5ec6344f3ede952cfb6bf942ab095948bfa4ca5d
SergiOn <Onishhenko@gmail.com>
2017-05-01 15:00:31 +0300

    #16 symbols # examples

--- src/symbols.js
+++ src/symbols.js
@@ -1,2 +1,5 @@
-let symbol = Symbol();
+let symbol = Symbol('name'); // 445gfg45fg4f
+let symbol2 = Symbol('name'); // fdfmnrmnfdln
 console.log(symbol);
+console.log(symbol2);
+console.log(symbol === symbol2);
